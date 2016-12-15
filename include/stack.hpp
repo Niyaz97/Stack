c3042df4f6fcbcd04b252e04407618c0b0fe8987
Niyaz97 <niyaz160297@gmail.com>
2016-12-15 11:17:20 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -4,58 +4,147 @@
 #include <iostream>
 
 template <typename T>
-class stack {
-public:
+class allocator{
+protected:
+    allocator(size_t size=0);
+    allocator(allocator const&) = delete;
+    ~allocator();
 
-    stack();
-    ~stack();
+    auto swap(allocator& other) -> void;
+    auto operator =(allocator const&) -> allocator& = delete;
 
-    auto count() const noexcept -> size_t ;
-    auto push(T const& value) -> void;
-    auto pop() -> T;
-
-
-private:
-    T * array_;
-    size_t array_size_;
+    auto allocate(const size_t) -> void;
+    auto deallocate() -> void;
+    T * ptr_;
+    size_t size_;
     size_t count_;
 };
 
 
 template <typename T>
-stack<T>::stack() : count_(0), array_(nullptr),  array_size_(0) {
+allocator<T>::allocator(size_t size) {
+    size_=0;
+    count_=size;
+    ptr_=static_cast<T*>(::operator new(size * sizeof(T)));
 }
 
 template <typename T>
-stack<T>::~stack() {
-    delete[] array_;
+allocator<T>::~allocator() {
+    for(int i = 0; i < count_; ++i)
+        ptr_[i].~T();
+    ::operator delete(ptr_);
 }
+
+template <typename T>
+auto allocator<T>::swap(allocator &other) ->void {
+    std::swap(ptr_, other.ptr_);
+    std::swap(count_, other.count_);
+    std::swap(size_, other.size_);
+}
+
+
+template <typename T>
+auto allocator<T>::allocate(const size_t st_size) -> void {
+
+    allocator<T> temp(st_size);
+
+    try {
+        temp.count_ = count_;
+        std::copy(ptr_, ptr_ + size_, temp.ptr_);
+    }
+    catch(...) {
+        throw;
+    }
+    swap(temp);
+}
+
+template <typename T>
+auto allocator<T>::deallocate() -> void {
+    allocator<T> temp(size_);
+    std::copy(ptr_, ptr_ + count_, temp.ptr_);
+    swap(temp);
+}
+
+template <typename T>
+class stack : private allocator<T> {
+public:
+
+    stack(size_t size = 0);
+
+    auto count() const noexcept -> size_t ;
+    auto push(T const& value) /* strong */ -> void;
+
+    auto top() const /* strong */ -> T;
+    auto pop() noexcept -> void;
+
+    auto empty() const noexcept -> bool;
+};
+
+
+template <typename T>
+stack<T>::stack(size_t size): allocator<T>(size) {
+}
+
 
 template <typename T>
 auto stack<T>::count() const noexcept -> size_t {
-    return count_;
+    return allocator<T>::count_;
 }
 
 template <typename T>
-auto stack<T>::push(T const& value) -> void {
-    if (count_ == array_size_) {
-        size_t size = array_size_ * 2 + (array_size_ == 0);
-        T * m_array = new T[size];
-        std::copy(array_, array_ + array_size_, m_array);
-        delete[] array_;
-        array_ = m_array;
-        array_size_ = size;
+auto stack<T>::empty() const noexcept -> bool {
+    return (allocator<T>::count_==0);
+}
+
+template <typename T>
+auto stack<T>::push(T const& value) /* strong */ -> void {
+    T* s_array = allocator<T>::ptr_;
+    bool  flag = false;
+    size_t st_size = allocator<T>::size_ * 2 + (allocator<T>::size_ == 0);
+
+    if (allocator<T>::count_ == allocator<T>::size_) {
+        try {
+            allocator<T>::allocate(st_size);
+            flag = true;
+        }
+        catch (...) {
+            throw;
+        }
     }
-    ++count_;
-    array_[count_ - 1] = value;
+    try {
+        allocator<T>::ptr_[allocator<T>::count_] = value;
+    }
+    catch(...) {
+        if(flag) allocator<T>::allocate(st_size/2);
+        throw;
+    }
+    allocator<T>::count_++;
 }
 
 template <typename T>
-auto stack<T>::pop() -> T{
-    if(count_==0)
-        throw std::logic_error("stack is empty!");
-
-    return array_[--count_];
+auto stack<T>::top() const /* strong */ -> T try {
+    if(allocator<T>::count_== 0)
+        throw std::logic_error("Error! Stack is empty!");
+    return allocator<T>::ptr_[allocator<T>::count_-1];
 }
+catch(std::logic_error& err) {
+    std::cout << err.what() << std::endl;
+}
+catch(...) {
+    throw;
+}
+
+template <typename T>
+auto stack<T>::pop() noexcept  -> void try  {
+    if(allocator<T>::count_==  0)
+        throw std::logic_error("Error! Stack is empty!");
+
+    else --allocator<T>::count_;
+}
+catch(std::logic_error& err)
+{
+    std::cout << err.what() << std::endl;
+}
+
 
 #endif
