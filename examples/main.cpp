a8a257461d9208f7be6b9d318e54951b9eedfcc6
Niyaz97 <niyaz160297@gmail.com>
2016-12-15 11:17:59 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -4,22 +4,27 @@ int main() {
 
     stack<size_t> st;
 
+
     try {
         st.push(1);
         st.push(2);
         st.push(3);
-    
-        std::cout << "Size of stack "<< st.count() << std::endl;
 
-        std::cout << st.pop() << std::endl;
-        std::cout << st.pop() << std::endl;
-        std::cout << st.pop() << std::endl;
+        std::cout << "Size of stack " << st.count() << std::endl;
+        std::cout << st.top() << std::endl;
+        st.pop();
+        std::cout << st.top() << std::endl;
+        st.pop();
+        std::cout << st.top() << std::endl;
+        st.pop();
+
     }
     catch (std::logic_error& e) {
-        std::cerr << e.what() << std::endl;
+        std::cout << e.what() << std::endl;
     }
 
-    std::cout << "Size of stack after popping elements "<< st.count() << std::endl;
+    std::cout << "Size of stack after popping elements " << st.count() << std::endl;
+    if(st.empty()) std::cout<< "Stack is empty" << std::endl;
 
     return 0;
 }
