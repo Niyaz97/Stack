620c0123eec4ce63e6d6af1f91811d722eb4b75e
Niyaz97 <niyaz160297@gmail.com>
2016-12-18 18:10:45 +0300

    Update stack.hpp

--- include/stack.hpp
+++ include/stack.hpp
@@ -1,67 +1,14 @@
 #ifndef STACK_STACK_HPP
 #define STACK_STACK_HPP
 
-#include <iostream>
-
-template <typename T>
-class allocator{
-protected:
-    allocator(size_t size=0);
-    allocator(allocator const&) = delete;
-    ~allocator();
-
-    auto swap(allocator& other) -> void;
-    auto operator =(allocator const&) -> allocator& = delete;
-
-    auto allocate(const size_t) -> void;
-    T * ptr_;
-    size_t size_;
-    size_t count_;
-};
+#include <allocator.hpp>
 
 
 template <typename T>
-allocator<T>::allocator(size_t size) {
-    size_=0;
-    count_=size;
-    ptr_=static_cast<T*>(::operator new(size * sizeof(T)));
-}
-
-template <typename T>
-allocator<T>::~allocator() {
-    for(int i = 0; i < count_; ++i)
-        ptr_[i].~T();
-    ::operator delete(ptr_);
-}
-
-template <typename T>
-auto allocator<T>::swap(allocator &other) ->void {
-    std::swap(ptr_, other.ptr_);
-    std::swap(count_, other.count_);
-    std::swap(size_, other.size_);
-}
-
-
-template <typename T>
-auto allocator<T>::allocate(const size_t st_size) -> void {
-
-    allocator<T> temp(st_size);
-
-    try {
-        temp.count_ = count_;
-        std::copy(ptr_, ptr_ + size_, temp.ptr_);
-    }
-    catch(...) {
-        throw;
-    }
-    swap(temp);
-}
-
-
-template <typename T>
-class stack : private allocator<T> {
+class stack {
 public:
 
+    explicit
     stack(size_t size = 0);
 
     auto count() const noexcept -> size_t ;
@@ -71,54 +18,53 @@ public:
     auto pop() noexcept -> void;
 
     auto empty() const noexcept -> bool;
+
+private:
+    allocator<T> alloc;
 };
 
 
 template <typename T>
-stack<T>::stack(size_t size): allocator<T>(size) {
+stack<T>::stack(size_t size): alloc(size) {
 }
 
 
 template <typename T>
 auto stack<T>::count() const noexcept -> size_t {
-    return allocator<T>::count_;
+    return alloc.count();
 }
+
 
 template <typename T>
 auto stack<T>::empty() const noexcept -> bool {
-    return (allocator<T>::count_==0);
+    return (alloc.count() == 0);
 }
 
 template <typename T>
 auto stack<T>::push(T const& value) /* strong */ -> void {
-    T* s_array = allocator<T>::ptr_;
-    bool  flag = false;
-    size_t st_size = allocator<T>::size_ * 2 + (allocator<T>::size_ == 0);
 
-    if (allocator<T>::count_ == allocator<T>::size_) {
+    if (alloc.empty() || alloc.full()) {
         try {
-            allocator<T>::allocate(st_size);
-            flag = true;
+            alloc.resize();
         }
         catch (...) {
             throw;
         }
     }
+
     try {
-        allocator<T>::ptr_[allocator<T>::count_] = value;
+        alloc.construct(alloc.get() + alloc.count(), value);
     }
     catch(...) {
-        if(flag) allocator<T>::allocate(st_size/2);
         throw;
     }
-    allocator<T>::count_++;
 }
 
 template <typename T>
 auto stack<T>::top() const /* strong */ -> T try {
-    if(allocator<T>::count_== 0)
+    if(alloc.count() == 0)
         throw std::logic_error("Error! Stack is empty!");
-    return allocator<T>::ptr_[allocator<T>::count_-1];
+    return alloc.get()[alloc.count()-1];
 }
 catch(std::logic_error& err) {
     std::cout << err.what() << std::endl;
@@ -129,13 +75,12 @@ catch(...) {
 
 template <typename T>
 auto stack<T>::pop() noexcept  -> void try  {
-    if(allocator<T>::count_==  0)
+    if(alloc.count() ==  0)
         throw std::logic_error("Error! Stack is empty!");
 
-    else --allocator<T>::count_;
+    else alloc.destroy(alloc.get() + alloc.count() - 1);
 }
-catch(std::logic_error& err)
-{
+catch(std::logic_error& err) {
     std::cout << err.what() << std::endl;
 }
 
